77cfc437ece5468d389596ca856413e73da86fcd
MODIFICATOR-PC\modif_000 <modificatorX@mail.ru>
2017-11-14 09:36:56 +0700

    Добавьте файлы проекта.

---
+++ WebApplication2/Startup.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using WebApplication2.Data;
+using WebApplication2.Models;
+using WebApplication2.Services;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication2
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets<Startup>();
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            using (var context = scope.ServiceProvider.GetService<ApplicationDbContext>())
+            {
+                context.Database.Migrate();
+                this.ConfigureIdentity(scope).GetAwaiter().GetResult();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseIdentity();
+
+            // Add external authentication middleware below. To configure them please see https://go.microsoft.com/fwlink/?LinkID=532715
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+        private async Task ConfigureIdentity(IServiceScope scope)
+        {
+            var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
+            var userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+
+            var adminsRole = await roleManager.FindByNameAsync(ApplicationRoles.Administrators);
+            if (adminsRole == null)
+            {
+                var roleResult = await roleManager.CreateAsync(new IdentityRole(ApplicationRoles.Administrators));
+                if (!roleResult.Succeeded)
+                {
+                    throw new InvalidOperationException($"Unable to create {ApplicationRoles.Administrators} role.");
+                }
+
+                adminsRole = await roleManager.FindByNameAsync(ApplicationRoles.Administrators);
+            }
+
+            var adminUser = await userManager.FindByNameAsync("admin");
+            if (adminUser == null)
+            {
+                var userResult = await userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "admin"
+                    
+                }, "AdminPass123!");
+                if (!userResult.Succeeded)
+                {
+                    throw new InvalidOperationException($"Unable to create admin");
+                }
+
+                adminUser = await userManager.FindByNameAsync("admin");
+            }
+
+            if (!await userManager.IsInRoleAsync(adminUser, adminsRole.Name))
+            {
+                await userManager.AddToRoleAsync(adminUser, adminsRole.Name);
+            }
+        }
+    }
+}
